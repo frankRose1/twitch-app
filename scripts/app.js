8471cb792c5344c54fd2a8ad33a1edd7357fca05
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-06-12 15:52:43 -0400

    added some more styling and set up the API call

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,18 @@
+//get JSON from twitch API
+	//set up functions to handle the data
+		//update the grid items
+		//change styles based on channel status
+			//starter info needed: 
+				//channel logo, channel name, status
 $(document).ready(function(){
+
+	const $gridItem = ('.grid-item')
+	const URL = 'https://wind-bow.gomix.me/twitch-api';
+
+	function getData(){
+		$.getJSON(URL, function(){
+			console.log(data);
+		});
+	}
 
 });
