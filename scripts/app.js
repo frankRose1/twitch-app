440550eec42d1d23bf55b66323d133004f02a9fe
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-06-14 19:28:08 -0400

    added code to update the DOM with response from the API

--- scripts/app.js
+++ scripts/app.js
@@ -4,15 +4,61 @@
 		//change styles based on channel status
 			//starter info needed: 
 				//channel logo, channel name, status
+				//"https://api.twitch.tv/kraken/streams?channel=streamer1,streamer2,streamer3,streamer4&client_id=<<your client ID>>"
 $(document).ready(function(){
 
-	const $gridItem = ('.grid-item')
-	const URL = 'https://wind-bow.gomix.me/twitch-api';
+	const $gridItem = ('.grid-item');
 
+// $.ajax({
+// 	 type: 'GET',
+// 	 url: 'https://api.twitch.tv/kraken/channels/twitch',
+// 	 headers: {
+// 	   'Client-ID': 'axjhfp777tflhy0yjb5sftsil'
+// 	 },
+// 	 success: function(data) {
+// 	   console.log(data);
+// 	 }
+// }); 
 	function getData(){
-		$.getJSON(URL, function(){
-			console.log(data);
+		$.ajax({
+			type: 'GET',
+			url: `https://api.twitch.tv/kraken/streams/featured?limit=12`,
+			headers: {
+				'Client-ID': 'mnjxuz1js2d8ix4azjyvpa656pi5w9'
+			},
+			success: addStreamers,
+			error: handleError
 		});
+	}
+
+	function addStreamers(data){
+		console.log(data.featured);
+		const streams = data.featured;
+		streams.forEach(function(streamer){
+			updateDOM(streamer);
+		});
+	}
+
+	function updateDOM(streamer){
+		//create a grid item for each stream response
+		const logo = streamer.stream.channel.logo;
+		const name = streamer.stream.channel.display_name;
+		const status = streamer.stream.channel.status;
+
+		const newGridItem = $(`<div class="grid-item">
+	                    			<img src="${logo}" alt="Streamer Logo" class="streamer-logo">
+	                    			<div class="channel-info">
+	                        			<h3 class="name">${name}</h3>
+	                        			<p class="status">${status}</p>
+	                    			</div>
+         						</div>`);
+		newGridItem.data('id', streamer._id);
+		$('#grid').append(newGridItem);
+	}
+
+	//use this to display an error in the DOM
+	function handleError(err){
+		console.log(err);
 	}
 
 	//smooth scroll
@@ -21,8 +67,9 @@ $(document).ready(function(){
 		const target = $(this.hash);
 		
 		$('html, body').stop().animate({
-			scrollTop: target.offset().top -60
+			scrollTop: target.offset().top
 		}, 750);
 	});
 
+getData();
 }); //end doc ready
