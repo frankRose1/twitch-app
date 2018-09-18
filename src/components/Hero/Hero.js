7c8010acfef372ab3280162c7449eaa0f4a90d6e
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-09-18 12:28:42 -0400

    Add smooth scroll

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Hero = () => {
     return (
@@ -6,7 +7,7 @@ const Hero = () => {
             <div className="container">
                 <h1>twitch.tv</h1>
                 <div className="arrow">
-                    <a href="#content" className="down-arr"><i className="fas fa-arrow-circle-down"></i></a>
+                    <AnchorLink href="#content" className="down-arr"><i className="fas fa-arrow-circle-down"></i></AnchorLink>
                 </div>
             </div>
         </section>
